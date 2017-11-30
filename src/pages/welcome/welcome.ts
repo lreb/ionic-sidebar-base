d17a62d717fd380c6957c8f1af85fda21933909c
Luis Raul Espinoza Barboza <luis.espinoza@buildbinder.com>
2017-11-30 16:00:16 -0700

    configuring browser and android platform to use and test g+

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { SignInPage } from '../sign-in/sign-in';
 import { SignUpPage } from '../sign-up/sign-up';
-
+import { GooglePlus } from '@ionic-native/google-plus';
 /**
  * Generated class for the WelcomePage page.
  *
@@ -16,7 +16,7 @@ import { SignUpPage } from '../sign-up/sign-up';
 })
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private googlePlus: GooglePlus) {
   }
 
   ionViewDidLoad() {
@@ -29,7 +29,13 @@ export class WelcomePage {
   	this.navCtrl.push(SignUpPage);
   }
   google(){
-
+    this.googlePlus.login({
+          'webClientId': '780469554734-rpkej3o37on3p5maksp9klqjvj5hg0k7.apps.googleusercontent.com'
+        }).then((res) => {
+            console.log(res);
+        }, (err) => {
+            console.log(err);
+        });
   }
   facebook(){
 
