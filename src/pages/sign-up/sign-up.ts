1b80040b159b9a739d679255c8490713aba01187
Luis Raul Espinoza Barboza <luis.espinoza@buildbinder.com>
2017-12-04 15:35:04 -0700

    form validator in signup

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Events } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators, FormControl, ValidatorFn, AbstractControl } from '@angular/forms';
 /**
  * Generated class for the SignUpPage page.
  *
@@ -11,11 +12,19 @@ import { NavController, NavParams, Events } from 'ionic-angular';
   selector: 'page-sign-up',
   templateUrl: 'sign-up.html',
 })
-export class SignUpPage {
+//let emailRegex = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
+export class SignUpPage implements Validators {
+  form : FormGroup;
 
   constructor(public navCtrl: NavController, 
   	public navParams: NavParams, 
-  	public events: Events) {
+  	public events: Events,
+  	public formBuilder: FormBuilder) {
+  	this.form = formBuilder.group({
+  		emailVal: ['', Validators.compose([Validators.required, Validators.email])],
+  		passwordVal: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+  		passwordConfirmVal: ['', Validators.compose([Validators.required, Validators.minLength(5), passwordValidator.passwordMatch('passwordVal')])]
+  	});
   }
 
   ionViewDidLoad() {
@@ -27,4 +36,17 @@ export class SignUpPage {
   	this.events.publish('user:register', {mail:'holo', password: '123456', confirm_password: '123456'});
   }
 
+}
+/** verify if passwords are equals */
+export class passwordValidator {
+  static passwordMatch(field_name): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} => {
+      let input = control.value;
+      let isValid = control.root.value[field_name] == input;
+      if (!isValid)
+        return {'equalTo': {isValid}};
+      else
+        return null;
+    };
+  }
 }
